d797a7c769dda29a13af1728c94893b333af7285
ricardo rocha <lericardolima@gmail.com>
2020-02-15 22:36:43 -0300

    Map the model Task with sequelize

---
+++ server/migrations/20200216012145-create-task.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ['pending', 'ongoing', 'completed'],
+        defaultValue: 'pending'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Tasks');
+  }
+};
---
+++ server/models/task.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Task = sequelize.define('Task', {
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM({
+        values: ['pending', 'ongoing', 'completed']
+      }),
+      defaultValue: 'pending'
+    }
+  }, {});
+  Task.associate = function(models) {
+    // associations can be defined here
+  };
+  return Task;
+};
