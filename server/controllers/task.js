b2ec74c31d63d5ddace8728e86da464049779900
Ricardo Rocha <ricardo.rocha@mv.com.br>
2020-03-04 16:25:23 -0300

    Improve code coverage

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -11,8 +11,7 @@ exports.create = (req, res) => {
 
 exports.list = (req, res) => {
     return Task.findAll()
-                    .then(tasks => res.status(200).send(tasks))
-                    .catch(error => res.status(400).send(error));
+                    .then(tasks => res.status(200).send(tasks));
 }
 
 exports.get = (req, res) => {
@@ -29,19 +28,19 @@ exports.get = (req, res) => {
 }
 
 exports.update = (req, res) => {
-    return Task.findByPk(req.params.taskId)
-                    .then(task => {
-                        if (!task)
+    return Task.update({
+                            description: req.body.description,
+                            status: req.body.status
+                        },
+                        { returning: true, where: {id: req.params.taskId}
+                    })
+                    .then(([rows, [task]]) => {
+                        if (!rows)
                             return res.status(404).send({
                                 message: 'Task not found'
                             });
 
-                        return task.update({
-                            description: req.body.description,
-                            status: req.body.status
-                        })
-                        .then(() => res.status(200).send(task))
-                        .catch(error => res.status(400).send(error));
+                        return res.status(200).send(task);
                     })
                     .catch(error => res.status(400).send(error));
 }
@@ -55,8 +54,7 @@ exports.delete = (req, res) => {
                             });
 
                         return task.destroy()
-                                    .then(() => res.status(204).send(task))
-                                    .catch(error => res.status(400).send(error));
+                                    .then(() => res.status(204).send(task));
                     })
                     .catch(error => res.status(400).send(error));
 }
